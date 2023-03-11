34b8b11594c64d038b6d0521f30afcdb34b56d6d
SHLEM666 <shlem666@yandex.ru>
2023-03-11 10:47:02 +0300

    Update Mathprofi.js

--- Userscripts/Mathprofi.js
+++ Userscripts/Mathprofi.js
@@ -1,29 +1,28 @@
 // ==UserScript==
-// @name         Mathprofi
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://www.mathprofi.ru/*
-// @grant        none
+// @name         Mathprofi
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.mathprofi.ru/*
+// @match        http://mathprofi.ru/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mathprofi.ru
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-	
-    // Your code here...
-	
-    // Получаем ширину экрана
-	var scr_wdth = document.documentElement.clientWidth - 50;
 
-	// Получаем главный фрейм
-	var main = document.getElementsByTagName('table')[0].children[0].children[1].children[1];
+	// Получаем рекламу
+    var ads = document.getElementsByTagName('NOINDEX');
 
-	// Прячим рекламу
-	if ( main.children[0].tagName == 'NOINDEX') {
-	    
-	    main.children[0].style.display = 'none';
-    };
+    // Прячим рекламу
+    [].forEach.call(ads, function(ad, i) {
+        ad.style.display = "none";
+    });
+
+    // Получаем главный фрейм
+    var main = document.getElementsByTagName('table')[0].children[0].children[1].children[1];
 
 	// Получаем меню
 	var menu = document.getElementsByTagName('table')[0].children[0].children[1].children[0];
@@ -31,8 +30,14 @@
 	// Прячим меню
 	menu.style.display = "none";
 
+    // Расширяем меню
+    menu.width = "";
+
 	// Увеличиваем изображения
 	(function change_img_dims() {
+
+        // Получаем ширину экрана
+        var scr_wdth = document.documentElement.clientWidth - 50;
 
 	    // Получаем все изображения
 	    var imgs = main.getElementsByTagName('img');
@@ -42,10 +47,10 @@
 
 	    [].forEach.call(imgs, function(img, i) {
 
-	        var a = 3;
+	        var factor = 3;
 
-	        new_width = img.width * a;
-	        new_height = img.height * a;
+	        new_width = img.width * factor;
+	        new_height = img.height * factor;
 
 	        if (new_width > scr_wdth) {
 
@@ -62,6 +67,26 @@
 	    });
 	})();
 
+    // Создаем стили
+    (function add_style() {
+
+        var style = document.createElement('style');
+
+        // Внешний вид кнопки
+        style.innerHTML += '.my_button {background-color:#0000004d; border-radius:100%; top: 30px; right: 30px; position: fixed; height:100px; width:100px; font-size: 65pt; text-align: center; color: white;}';
+
+        // Размеры контейнеров меню
+        style.innerHTML += '#bar, #bar1 {height: auto; width: 100%;}';
+
+        // Увеличиваем шрифт ссылок в меню
+        style.innerHTML += 'a.classbar {display: block;}';
+
+        // Увеличиваем шрифт заголовков в меню
+        style.innerHTML += 'p.classtopic {margin: 75px 0px 35px 0px; font-size: 20pt;}';
+
+        document.body.appendChild(style);
+    })();
+
 	// Создаем кнопку меню
 	(function add_my_button() {
 
@@ -70,30 +95,21 @@
 	    my_button.className = 'my_button';
 	    my_button.onclick = toggle_menu;
 
-	    my_button.innerHTML += '';
+	    my_button.innerHTML += '≡';
 
 	    document.body.appendChild(my_button);
-
-	    add_style();
-
-	    function add_style() {
-
-	        var style = document.createElement('style');
-
-	        style.innerHTML += '.my_button {background-color:#0000004d; border-radius:100%; top: 0px; left:0px; position: fixed;height:100px; width:100px;}';
-
-	        document.body.appendChild(style);
-	    };
 
 	    function toggle_menu() {
 
 	        if (menu.style.display == "") {
 
 	            menu.style.display = "none";
+                main.style.display = "";
 
 	        } else {
 
 	            menu.style.display = "";
+                main.style.display = "none";
 	        };
 	    };
 	})();
