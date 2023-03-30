947ddca741ea7519fc70ad1b0b68a58b3688d503
SHLEM666 <shlem666@yandex.ru>
2023-03-30 23:10:11 +0300

    Update GregsWiki.js

--- GregsWiki.js
+++ GregsWiki.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *mywiki.wooledge.org/*
+// @match        *.mywiki.wooledge.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wooledge.org
 // @grant        none
 // @require      https://raw.githubusercontent.com/SHLEM666/Userscripts/main/GregsWiki.js
